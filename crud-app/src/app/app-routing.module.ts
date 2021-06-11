9472273eb1feb6e3a9a653a4e121533616bd5666
Ghost Tall <diarnotall@gmail.com>
2021-06-11 12:10:28 -0700

    installation des framework css

--- crud-app/src/app/app-routing.module.ts
+++ crud-app/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './composants/home/home.component';
+import { LoginComponent } from './composants/login/login.component';
+import { RegisterComponent } from './composants/register/register.component';
+import { ResetComponent } from './composants/reset/reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "reset",
+    component: ResetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud-app/src/app/app.module.ts
+++ crud-app/src/app/app.module.ts
@@ -4,15 +4,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './composants/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './composants/login/login.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { BaseComponent } from './core/base/base.component';
+import { RegisterComponent } from './composants/register/register.component';
+import { ResetComponent } from './composants/reset/reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    BaseComponent,
+    RegisterComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
