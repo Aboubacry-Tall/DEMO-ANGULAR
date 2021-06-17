c1c515136c06b03fa62aa83422a1584ccb37c73e
Ghost Tall <diarnotall@gmail.com>
2021-06-17 13:16:35 -0700

    Import module Menu

--- crud-app/src/app/app.module.ts
+++ crud-app/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
 import { HomeComponent } from './composants/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './composants/login/login.component';
@@ -72,6 +73,7 @@ import { UsersComponent } from './modules/users/users/users.component';
     MatCardModule,
     MatInputModule,
     MatExpansionModule,
+    MatMenuModule,
     HttpClientModule
   ],
   providers: [],
