0130ee482bbb66b4bd3c38c06cf3e5a6ac1e2089
Cheikh22 <84624356+Cheikh22@users.noreply.github.com>
2021-06-19 11:50:25 +0000

    fix register

--- crud-app/src/app/composants/login/login.component.ts
+++ crud-app/src/app/composants/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
         this.msg='Email ou mot de passe invalide';
     }
     goToUser(){
-      this.router.navigate(['']);
+      this.router.navigate(['home']);
     }
     goToAdmin(){
       this.router.navigate(['admin']);
