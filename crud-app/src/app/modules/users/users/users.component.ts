a09201d869a8bb10924a81c43d4b64c252ac66b4
Cheikh22 <84624356+Cheikh22@users.noreply.github.com>
2021-06-22 12:40:43 +0000

    methods for research operations

--- crud-app/src/app/modules/users/users/users.component.ts
+++ crud-app/src/app/modules/users/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/user';
+import { UserDataService } from '../services/user-data.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
-
+  users: User[] = [];
+  user: User = new User();
+  nom='';
+  constructor(private userService: UserDataService,private router: Router) { }
   ngOnInit(): void {
+    
   }
+  
+  updateUser(id?:number){
+    this.router.navigate(['user',id]);
+  }
+  Activer(id?:number, user?: User){
+    this.userService.Activate(id,user).subscribe(data =>{
+    })
+    this.router.navigate(['admin-users'])
+  }
+  Suspendre(id?:number, user?: User){
+    this.userService.Suspended(id,user).subscribe(data =>{
+    })
+    this.router.navigate(['admin-users'])
+  }
+  Rechercher(){
+    this.userService.findUser(this.nom).subscribe(data =>{
+      this.users=data;
+    });
+  }
+  onSearch(event:any){
+    this.userService.findUser(this.nom).subscribe(data =>{
+      this.users=data;
+    });
+
+  }
+
 
 }
